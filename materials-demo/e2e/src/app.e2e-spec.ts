eb4b8adff429c78077523967af54ff986956aae9
Shantanu Banerjee <sbtalk@gmail.com>
2020-12-24 16:20:22 +0530

    final

--- materials-demo/e2e/src/app.e2e-spec.ts
+++ materials-demo/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('welcome to Angular testing');
+    expect(await page.getTitleText()).toEqual('materials-demo app is running!');
   });
 
   afterEach(async () => {
--- materials-demo/karma.conf.js
+++ materials-demo/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/testing'),
+      dir: require('path').join(__dirname, './coverage/materials-demo'),
       subdir: '.',
       reporters: [
         { type: 'html' },
